e9e62465038c2b1a8dba840260c56462b41463c4
B1Ib0 <75206537+B1Ib0@users.noreply.github.com>
2023-10-06 09:48:55 +0100

    Section 2: Scripting

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PickUp")
+        {
+            count++;
+            other.gameObject.SetActive(false);
+        }
+    }
+}
