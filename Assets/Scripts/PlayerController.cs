033b0c6d082fdf1805c0cc0be065f3adf442c3e8
B1Ib0 <75206537+B1Ib0@users.noreply.github.com>
2023-10-07 12:25:18 +0100

    Revert "Bhavan sections"
    This reverts commit b404aac0f8846170a9ce96ef5f22a9d84fdb183a.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,43 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.UI;
-using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
 
-    public Vector2 moveValue;
-
-    public float speed;
     private int count;
     private float startPos;
     private float newPos;
-    private int numPickups = 8;
-    public TextMeshProUGUI scoreText;
-    public TextMeshProUGUI winText;
-
-
-    void onMove(InputValue value)
-    {
-        moveValue = value.Get<Vector2>();
-    }
-
-    void FixedUpdate()
-    {
-        Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
-        
-        GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
-
-    }
 
     // Start is called before the first frame update
     void Start()
     {
         count = 0;
-        winText.text = "";
-        SetCountText ();
     }
 
     // Update is called once per frame
@@ -50,19 +25,8 @@ public class PlayerController : MonoBehaviour
     {
         if (other.gameObject.tag == "PickUp")
         {
-            
-            other.gameObject.SetActive(false);
             count++;
-            SetCountText();
-        }
-    }
-
-    private void SetCountText()
-    {
-        scoreText.text = "Score: " + count.ToString();
-        if(count >= numPickups)
-        {
-            winText.text = "You win!";
+            other.gameObject.SetActive(false);
         }
     }
 }
